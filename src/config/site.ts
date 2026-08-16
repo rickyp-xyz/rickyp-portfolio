0621e058f1463b421f11fc2fd350a3fd8876df73
Richard Szilagyi <riszilagyi@proton.me>
2026-08-16 09:01:31 +0700

    update global styles

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,12 +3,14 @@ export const site = {
     description: "I'm a software developer interested in building useful, reliable, and well-designed software.",
     location: "Thailand",
     url: "https://rickyp.xyz",
+    image: null,
     contacts: {
         email: "riszilagyi0@gmail.com",
         telegram: "",
     },
     socials: {
         github: "",
+        linkedin: "",
         twitter: "",
         youtube: ""
     }
