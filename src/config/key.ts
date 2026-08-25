457b42dff116aa7d69b22d6aeaa0b5a3d3191fdf
Richard Szilagyi <riszilagyi@proton.me>
2026-08-25 23:04:42 +0700

    change public key path from relative to absolute

--- src/config/key.ts
+++ src/config/key.ts
@@ -1,5 +1,4 @@
 export const key = {
-    "name": "Richard Szilagyi",
     "fingerprint": "FF5E 7195 EFF7 FE5B B85F D6FE 9A1A F9B8 73D9 DB82",
-    "publicKey": `./pgp/public-key.asc`
+    "publicKey": `/pgp/public-key.asc`
 }
